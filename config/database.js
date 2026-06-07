3913100132716ecffe2289b368cdfce286ba2b46
prependra <prependrasingh23646knit@gmail.com>
2026-06-08 00:41:04 +0530

    cc

--- config/database.js
+++ config/database.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL)
-    .then(() => console.log("DB Connected ho gya hai ram ram jii"))
-    .catch((error) => {
-        console.log("DB Connection Error");
-        console.error(error);
-    });
-};
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 module.exports = dbConnect;
